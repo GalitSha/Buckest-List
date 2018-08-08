0d0658c803ac0d3ba61cd034353cdc051ff882db
GalitSha <galit.sharon@mail.huji.ac.il>
2018-08-08 23:24:47 +0300

    Add git pages

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,7 +1,7 @@
 import {FETCH_ITEMS} from "../constants/action-types";
 
 export const fetchItems = () => async dispatch => {
-  let response = await fetch('http://roadmapservice.azurewebsites.net/api/buckets')
+  let response = await fetch('https://roadmapservice.azurewebsites.net/api/buckets')
   let data = await response.json();
   dispatch({type: FETCH_ITEMS, data: data})
 };
